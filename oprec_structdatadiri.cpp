f98b8d1b1b36624c902b4d0bf9fdb3a6babb5b3d
rama <117928757+alrazzh91@users.noreply.github.com>
2022-11-10 23:08:37 +0700

    Add files via upload

---
+++ oprec_structdatadiri.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char namadep[20]; char namabel[20]; int usia; int NIU;
+    
+    cout<<" ";
+    cin>>namadep;
+    cout<<" ";
+    cin>>namabel;
+    cout<<" ";
+    cin>>NIU;
+    cout<<" ";
+    cin>>usia;
+    
+    cout<<namadep<<" "<<namabel<<" "<<NIU<<" "<<usia<<endl;
+    
+    return 0;
+}
